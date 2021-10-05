60eeee49b11e8bf206c0df2a775842a947d7283b
nathalie loussouarn <natel@hotmail.com>
2021-10-05 22:00:25 +0200

    animation js banner

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+// animation banner elements
+const titleSpans = document.querySelectorAll('.title span');
+const medias = document.querySelectorAll('.connect');
+const img = document.getElementById('img-banner');
+
+window.addEventListener('load', () => {
+
+  const TL = gsap.timeline({paused: true});
+
+  TL
+  .staggerFrom(titleSpans, 1, {top: -50, opacity: 0, ease: "power2.out"}, 0.3)
+  .staggerFrom(img, 1, {left: -200, opacity:0, ease: "power2.out"}, 0.3, '-=2')
+  .from(medias, 1, {transform: "scale(0)", ease: "power2.out"}, '-=2');
+
+  TL.play();
+})
+
+
 //  animation etc
  const etc = document.getElementById('etc');
 
